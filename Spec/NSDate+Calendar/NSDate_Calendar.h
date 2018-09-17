bbce6239c1d4702038aaa06bc6c40f4dd0e5e20f
Jeremy Butler <jeremybutler@me.com>
2018-09-17 09:59:00 -0500

    Create dynamic framework

---
+++ Spec/NSDate+Calendar/NSDate_Calendar.h
@@ -0,0 +1,19 @@
+//
+//  NSDate_Calendar.h
+//  NSDate+Calendar
+//
+//  Created by Jeremy Butler on 2018/09/17.
+//  Copyright © 2018 Okolodev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSDate_Calendar.
+FOUNDATION_EXPORT double NSDate_CalendarVersionNumber;
+
+//! Project version string for NSDate_Calendar.
+FOUNDATION_EXPORT const unsigned char NSDate_CalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSDate_Calendar/PublicHeader.h>
+
+
